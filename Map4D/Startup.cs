a16cda67d8e67ae7a027f03e21db02a850a98a77
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-11-28 14:26:29 +0700

    start project

---
+++ Map4D/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Map4D.Startup))]
+namespace Map4D
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
