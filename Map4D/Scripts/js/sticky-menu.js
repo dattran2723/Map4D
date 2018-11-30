93f2465a64c69e80960ee1aae0ca395c4f13a882
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-11-30 17:34:49 +0700

    update page

---
+++ Map4D/Scripts/js/sticky-menu.js
@@ -0,0 +1,29 @@
+﻿window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
+        $("#menu-top nav").css("background-color","#3597e4");
+        $("#menu-top nav .logo a").css({
+            width: "50px",
+            height: "50px",
+            top: "-25px",
+        });
+        $("#menu-top").css("top", "-55px");
+        $("#btn-signin a").css({
+            width: "150px",
+            height: "40px"
+        });
+
+    } else {
+        $("#menu-top nav .logo a").css({
+            width: "90px",
+            height: "90px",
+            top: "-60px"
+        });
+        $("#menu-top").css("top", "0");
+        $("#menu-top nav").css("background-color", "");
+        $("#btn-signin a").css({
+            width: "160px",
+            height: "50px"
+        });
+    }
+};
