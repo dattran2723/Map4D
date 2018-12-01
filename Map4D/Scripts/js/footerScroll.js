a350c6cf02cd8e5cffd2e2c618c82a368a184e3d
Tran Dat <Tran Dat@DESKTOP-SLFVQSH>
2018-12-02 00:10:37 +0700

    add view MapBox.cshtml

---
+++ Map4D/Scripts/js/footerScroll.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function(){
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300) {
+            $('.scroll-to-top').fadeIn("slow");
+        } else {
+            $('.scroll-to-top').fadeOut("slow");
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scroll-to-top').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
--- Map4D/Scripts/js/sticky-menu.js
+++ Map4D/Scripts/js/sticky-menu.js
@@ -1,29 +1,30 @@
-﻿window.onscroll = function () { scrollFunction() };
-function scrollFunction() {
-    if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
-        $("#menu-top nav").css("background-color","#3597e4");
-        $("#menu-top nav .logo a").css({
-            width: "50px",
-            height: "50px",
-            top: "-25px",
-        });
-        $("#menu-top").css("top", "-55px");
-        $("#btn-signin a").css({
-            width: "150px",
-            height: "40px"
-        });
-
-    } else {
-        $("#menu-top nav .logo a").css({
-            width: "90px",
-            height: "90px",
-            top: "-60px"
-        });
-        $("#menu-top").css("top", "0");
-        $("#menu-top nav").css("background-color", "");
-        $("#btn-signin a").css({
-            width: "160px",
-            height: "50px"
-        });
-    }
-};
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $("#menu-top nav").css("background", "#3597e4");
+            $("#menu-top nav .logo").addClass("sticky");
+            $("#menu-top").css("top", "-55px");
+            $("#btn-signin").css({
+                width: "150px",
+                height: "40px"
+            });
+        }
+        else {
+            if (!$(".showMenu").hasClass("show")) {
+                $("#menu-top nav").css("background", "");
+            }
+            $("#menu-top nav").removeClass("bg-color");
+            $("#menu-top nav .logo").removeClass("sticky");
+            $("#menu-top").css("top", "0");
+            if ($(window).width() > 991) {
+                $("#btn-signin").css({
+                    width: "160px",
+                    height: "50px"
+                });
+            }
+        }
+    });
+    $("#btn-menu").click(function () {
+        $("#menu-top nav").toggleClass("bg-color");
+    });
+});
