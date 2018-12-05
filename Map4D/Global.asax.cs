b919e8fd4ad3d8f09572a4cb0ebad53ffa0273da
long đoàn <doanthanhlong1801@gmail.com>
2018-12-05 16:46:18 +0700

    commit model customer and add nuget automapper

--- Map4D/Global.asax.cs
+++ Map4D/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Map4D.Models;
 
 namespace Map4D
 {
@@ -12,6 +13,11 @@ namespace Map4D
     {
         protected void Application_Start()
         {
+            AutoMapper.Mapper.Initialize(conf =>
+            {
+                conf.CreateMap<CustomerListViewModels, CustomerModels>();
+                conf.CreateMap<CustomerRegisterViewModels, CustomerModels>();
+            });
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
