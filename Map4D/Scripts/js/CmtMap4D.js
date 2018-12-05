9853b8c24dd849a51cafea500e1ebcde0bd0da0d
long đoàn <doanthanhlong1801@gmail.com>
2018-12-05 10:59:12 +0700

    commit again responsive table

---
+++ Map4D/Scripts/js/CmtMap4D.js
@@ -0,0 +1,35 @@
+﻿var array = [
+    {
+        img: "~/Content/Client/img/product2.jpg",
+        name: "Nguyễn Văn Linh",
+        cmt: "Tính năng rất hữu ích và tiện dụng đối với tài xế chúng tôi"
+    },
+    {
+        img: "~/Content/Client/img/product2.jpg",
+        name: "Trần Văn Duy",
+        cmt: "Chúng tôi đặc biệt ấn tượng với chức năng hiển thị bản đồ theo thời gian"
+    },
+    {
+        img: "~/Content/Client/img/product2.jpg",
+        name: "Mr Robbert",
+        cmt: '"It is amazing"'
+    },
+    {
+        img: "~/Content/Client/img/product2.jpg",
+        name: "Nguyễn Thanh Hương",
+        cmt: "Map4D thể hiện đầy đủ các công nghệ hiện đại từ không gian ảo tới hình ảnh thực tế được xây dựng trên nền tảng 3D"
+    },
+    {
+        img: "~/Content/Client/img/product2.jpg",
+        name: "Trần Văn Qúy",
+        cmt: '"Good plans.Awesome work"'
+    },
+    {
+        img: "~/Content/Client/img/product2.jpg",
+        name: "Nguyễn Đăng Tài Hoa",
+        cmt: '"Good plans.Awesome work"'
+    },]
+for (var i = 0; i < array.length; i++) {
+
+
+}
